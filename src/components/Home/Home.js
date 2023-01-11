0a926706ea329632247df691ad4562295d9a796a
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2023-01-11 14:30:41 -0800

    preview rendering title changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,11 +8,10 @@ import './Home.css';
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
   const [title, setTitle] = useState('');
-  console.log('title', title);
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
+      <Preview title={title} />
       <Editor setTitle={setTitle} />
     </main>
   );
